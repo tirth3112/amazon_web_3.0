8f5ce61a6fe8f13f1cfca84b71b6a22fcdc7a788
Tirth Patel <tmp3199@gmail.com>
2022-05-07 17:33:05 -0400

    Update

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,16 +1,32 @@
 import React from 'react'
-import { ConnectButton } from 'web3uikit'
+import Image from 'next/image'
+import title from '../assests/a1.png'
+import { ConnectButton, Logo } from 'web3uikit'
+import Link from 'next/link'
+import { FaBox } from 'react-icons/fa'
+import { BsFillBookmarkFill } from 'react-icons/bs'
+import { BsFillPersonFill } from 'react-icons/bs'
+import { AiOutlineHistory } from 'react-icons/ai'
+import companytitle from '../assests/az3.gif'
+
 
 const isAuthenticated = true
-const username='Tirth'
+const username=''
 
 export const Sidebar = () => {
   const styles = {
       container: `h-full w-[300px] flex flex-col bg-[#fff] static`,
-      profile: ` w-full py-16 flex flex-col justify-center items-center rounded-r-3xl bg-gradient-to-t from-[#0d141c] to-[#42667e] mt-[40px] mb-[50px] border-2 border-[#fb9701]`,
+      profile: ` w-full  py-16 flex flex-col justify-center items-center rounded-r-3xl bg-gradient-to-t from-[#0d141c] to-[#42667e] mt-[40px] mb-[50px] border-2 border-[#fb9701]`,
       profilePicContainer: `flex  rounded-xl items-center justify-center w-full h-full mb-5`,
       profilePic: `rounded-3xl object-cover`,
       welcome: ` text-md mb-2 font-bold text-2xl text-white`,
+      menu: `flex flex-col w-full h-full px-10 gap-10`,
+      menuItem: `flex items-center text-lg font-bold cursor-pointer gap-2`,
+      amazonLogo: `mr-4 flex object-cover`,
+      companyName: `text-lg font-bold flex flex-1 pl-10 items-center mt-[20px]`,
+      usernameInput: `bg-transparent border-white border-2 rounded-lg w-[80%] py-2 px-4 text-lg mt-[20px] placeholder:text-white focus:outline-none flex justify-center items-center text-white`,
+      username: `flex items-center w-full justify-center`,
+      setNickname: `text-lg font-bold flex flex-1 items-center mt-[20px] mb-[20px] text-white`,
   }
   return (
     <div className={styles.container}>
@@ -18,13 +34,13 @@ export const Sidebar = () => {
         {isAuthenticated && (
           <>
             <div className={styles.profilePicContainer}>
-              <image
-                src={`https://avatars.dicebear.com/api/pixel-art/${username}.svg`}
-                alt='profile'
-                className={styles.profilePic}
-                height={100}
-                width={100}
-              />
+            <Image
+              src={`https://avatars.dicebear.com/api/pixel-art/${username}.svg`}
+              alt='profile'
+              className={styles.profilePic}
+              height={100}
+              width={100}
+            />
             </div>
             {!username ? (
               <>
@@ -33,13 +49,13 @@ export const Sidebar = () => {
                     type='text'
                     placeholder='Username....'
                     className={styles.usernameInput}
-                    value={nickname}
-                    onChange={e => setNickname(e.target.value)}
+                    // value={nickname}
+                    // onChange={e => setNickname(e.target.value)}
                   />
                 </div>
                 <button
                   className={styles.setNickname}
-                  onClick={handleSetUsername}
+                  // onClick={handleSetUsername}
                 >
                   Set Nickname
                 </button>
@@ -55,9 +71,49 @@ export const Sidebar = () => {
           <ConnectButton />
         </div>
       </div>
+      <div className={styles.menu}>
+      <Link href="index/">
+          <div className={styles.menuItem}>
+              <Image
+                src={title}
+                height={30}
+                width={30}
+                className={styles.amazonlogo}
+              />
+              My Amazon
+              <br /> board
+          </div>
+      </Link>
+         <div className={styles.menuItem}>
+              <FaBox/>
+              Collections
+         </div>
+         <div className={styles.menuItem}>
+              <BsFillBookmarkFill/>
+              Saved
+         </div>
+         <div className={styles.menuItem}>
+              <BsFillPersonFill/>
+              Profile
+         </div>
+      
+      <Link href= '/History'>
+         <div className={styles.menuItem}>
+              <AiOutlineHistory/>
+              Transcation History
+         </div>      
+      </Link>        
+    </div>
+    <div className={styles.companyName}>
+        <Image 
+        src={companytitle}
+        alt='amzon3.0'
+        height={170}
+        width={280}
+        />
+    </div>
 
       
     </div>
   )
 }
-export default Sidebar
